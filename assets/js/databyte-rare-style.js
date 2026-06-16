e36e68057c0e33eadbf84f8bcd5bfd91fbc9290d
voltanlabs <dennisbarnett16@gmail.com>
2026-06-16 18:34:56 -0500

    Load DataByteCoin container refinement layer

--- assets/js/databyte-rare-style.js
+++ assets/js/databyte-rare-style.js
@@ -1,7 +1,7 @@
 // assets/js/databyte-rare-style.js
 (function () {
   const NAMES = new Set(["Glitchwyrm", "Mirrormaster", "Proxsentience"]);
-  const FEATURE_VERSION = "0874-signal-collapse";
+  const FEATURE_VERSION = "0875-container-refine";
 
   function nameNow() {
     const el = document.getElementById("encounterName");
@@ -48,6 +48,7 @@
     retireLegacyBattle();
     loadScriptOnce("databyteBattlePhase2Loader", "/assets/js/databyte-battle-phase2.js");
     loadScriptOnce("databyteSignalCollapseLoader", "/assets/js/databyte-signal-collapse.js");
+    loadScriptOnce("databyteContainerRefineLoader", "/assets/js/databyte-container-refine.js");
     loadScriptOnce("databytePanelStateLoader", "/assets/js/databyte-panel-state.js");
     loadScriptOnce("databyteAppShellLoader", "/assets/js/databyte-app-shell.js");
     loadScriptOnce("databyteAdminConsoleLoader", "/assets/js/databyte-admin-console.js");
